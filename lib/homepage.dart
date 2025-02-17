f109c9c2bcb6df997f4bac340a5f764364802d0d
Riski Linardi <riskilinardi@gmail.com>
2025-02-18 03:44:46 +0800

    Added Navigation Bar and some testing boxes on homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,22 +1,74 @@
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-// class HomePage extends StatefulWidget {
-//   @override
-//   _HomePage createState() => _HomePage();
-// }
+class HomePage extends StatefulWidget {
+  @override
+  _HomePage createState() => _HomePage();
+}
 
-// class _HomePage extends State<HomePage> {
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: Container(
-//       color: Colors.white,
-//       child: Center(
-//         child: Column(
-//           children: <Widget>[Text("Welcome to Shiok Weather!")],
-//         ),
-//       ),
-//     ));
-//   }
-// }
+class _HomePage extends State<HomePage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                _buildTile(
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Weather 1',
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 10.0,
+                            ),
+                          ),
+                          Text(
+                            "Testing 123",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 34.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),],
+            ),
+          ),
+        ));
+  }
 
+  Widget _buildTile(Widget child, {Function()? onTap}) {
+    return Material(
+      elevation: 14.0,
+      borderRadius: BorderRadius.circular(12.0),
+      shadowColor: Color(0x802196F3),
+      child: InkWell(
+        // Do onTap() if it isn't null, otherwise do print()
+        onTap:
+        onTap != null
+            ? () => onTap()
+            : () {
+          print('Not set yet');
+        },
+        child: child,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,216 +1,67 @@
-// import 'package:Trackery/Animation/FadeAnimation.dart';
-// import 'package:Trackery/homepage.dart';
-// import 'package:Trackery/navbar.dart';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'navbar.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-// void main() => runApp(MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: LoginPage(),
-//     ));
-// // Future<void> main() async {
-// //   WidgetsFlutterBinding.ensureInitialized();
-// //   SharedPreferences prefs = await SharedPreferences.getInstance();
-// //   var name = prefs.getString('Name');
-// //   print(name);
-// //   runApp(MaterialApp(home: name == null ? LoginPage() : HomePage()));
-// // }
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    ));
 
-// class LoginPage extends StatefulWidget {
-//   @override
-//   _LoginPage createState() => _LoginPage();
-// }
+class MainPage extends StatefulWidget {
+  @override
+  _MainPage createState() => _MainPage();
+}
 
-// class _LoginPage extends State<LoginPage> {
-//   final TextEditingController _username = TextEditingController();
-//   final TextEditingController _password = TextEditingController();
+// NAVIGATION BAR CODES
+class _MainPage extends State<MainPage> {
+  int _page = 1;
+  final _pageOpt = [
+    HomePage(),
+    HomePage(),
+    HomePage()
+  ];
+  GlobalKey _bottomNavigationKey = GlobalKey();
 
-//   signIn(username, password) async {
-//     var url = 'https://napecinventory.azurewebsites.net/Api/LoginApi/' +
-//         username +
-//         '/' +
-//         password;
-//     var response = await http.get(url);
-
-//     if (response.statusCode == 200) {
-//       Navigator.of(context).pushAndRemoveUntil(
-//           MaterialPageRoute(builder: (BuildContext context) => NavBar()),
-//           (route) => false);
-//       print("Success");
-//     } else {
-//       print("Failed Login");
-//       AlertDialog alert = AlertDialog(
-//         title: Text("Error"),
-//         content: Text("Wrong Username or Password!"),
-//       );
-
-//       // show the dialog
-//       showDialog(
-//         context: context,
-//         builder: (BuildContext context) {
-//           return alert;
-//         },
-//       );
-//     }
-//   }
-
-//   String uid;
-//   String pw;
-
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         backgroundColor: Colors.white,
-//         body: SingleChildScrollView(
-//           child: Container(
-//             child: Column(
-//               children: <Widget>[
-//                 Container(
-//                   height: 400,
-//                   decoration: BoxDecoration(
-//                       image: DecorationImage(
-//                           image: AssetImage('assets/images/background.png'),
-//                           fit: BoxFit.fill)),
-//                   child: Stack(
-//                     children: <Widget>[
-//                       Positioned(
-//                         left: 30,
-//                         width: 80,
-//                         height: 200,
-//                         child: FadeAnimation(
-//                             1,
-//                             Container(
-//                               decoration: BoxDecoration(
-//                                   image: DecorationImage(
-//                                       image: AssetImage(
-//                                           'assets/images/light-1.png'))),
-//                             )),
-//                       ),
-//                       Positioned(
-//                         left: 140,
-//                         width: 80,
-//                         height: 150,
-//                         child: FadeAnimation(
-//                             1.3,
-//                             Container(
-//                               decoration: BoxDecoration(
-//                                   image: DecorationImage(
-//                                       image: AssetImage(
-//                                           'assets/images/light-2.png'))),
-//                             )),
-//                       ),
-//                       Positioned(
-//                         right: 40,
-//                         top: 40,
-//                         width: 80,
-//                         height: 150,
-//                         child: FadeAnimation(
-//                             1.5,
-//                             Container(
-//                               decoration: BoxDecoration(
-//                                   image: DecorationImage(
-//                                       image: AssetImage(
-//                                           'assets/images/clock.png'))),
-//                             )),
-//                       ),
-//                       Positioned(
-//                         child: FadeAnimation(
-//                           1.6,
-//                           Container(
-//                             margin: EdgeInsets.only(top: 140),
-//                             decoration: BoxDecoration(
-//                                 image: DecorationImage(
-//                                     image: AssetImage(
-//                                         'assets/images/TrackeryMobile.png'))),
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 Padding(
-//                   padding: EdgeInsets.all(30.0),
-//                   child: Column(
-//                     children: <Widget>[
-//                       FadeAnimation(
-//                           1.8,
-//                           Container(
-//                             padding: EdgeInsets.all(5),
-//                             decoration: BoxDecoration(
-//                                 color: Colors.white,
-//                                 borderRadius: BorderRadius.circular(10),
-//                                 boxShadow: [
-//                                   BoxShadow(
-//                                       color: Color.fromRGBO(143, 148, 251, .2),
-//                                       blurRadius: 20.0,
-//                                       offset: Offset(0, 10))
-//                                 ]),
-//                             child: Column(
-//                               children: <Widget>[
-//                                 Container(
-//                                   padding: EdgeInsets.all(8.0),
-//                                   decoration: BoxDecoration(
-//                                       border: Border(
-//                                           bottom: BorderSide(
-//                                               color: Colors.grey.shade100))),
-//                                   child: TextField(
-//                                       decoration: InputDecoration(
-//                                           border: InputBorder.none,
-//                                           hintText: "Username",
-//                                           hintStyle: TextStyle(
-//                                               color: Colors.grey[400])),
-//                                       controller: _username,
-//                                       onChanged: (value) {
-//                                         uid = value;
-//                                       }),
-//                                 ),
-//                                 Container(
-//                                   padding: EdgeInsets.all(8.0),
-//                                   child: TextField(
-//                                       decoration: InputDecoration(
-//                                         border: InputBorder.none,
-//                                         hintText: "Password",
-//                                       ),
-//                                       obscureText: true,
-//                                       controller: _password,
-//                                       onChanged: (value) {
-//                                         pw = value;
-//                                       }),
-//                                 )
-//                               ],
-//                             ),
-//                           )),
-//                       SizedBox(
-//                         height: 30,
-//                       ),
-//                       Container(
-//                         height: 40.0,
-//                         width: 250.0,
-//                         margin: EdgeInsets.all(10),
-//                         child: ElevatedButton(
-//                           child: Text('Login'),
-//                           style: ElevatedButton.styleFrom(
-//                               primary: Colors.indigo.shade500,
-//                               padding: EdgeInsets.symmetric(
-//                                   horizontal: 50, vertical: 0),
-//                               textStyle: TextStyle(
-//                                   fontSize: 20, fontWeight: FontWeight.bold)),
-//                           onPressed: () async {
-//                             try {
-//                               signIn(uid, pw);
-//                             } catch (e) {
-//                               print(e);
-//                             }
-//                           },
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 )
-//               ],
-//             ),
-//           ),
-//         ));
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Shiok Weather"),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[Colors.lightBlue, Colors.indigo]),
+            ),
+          ),
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
+          index: 1,
+          height: 50.0,
+          items: <Widget>[
+            Icon(MdiIcons.alphaI, size: 30),
+            Icon(MdiIcons.home, size: 30),
+            Icon(MdiIcons.alphaA, size: 30),
+          ],
+          color: Colors.indigo,
+          buttonBackgroundColor: Colors.indigo[400],
+          backgroundColor: Colors.white,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 600),
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
+          letIndexChange: (index) => true,
+        ),
+        body: _pageOpt[_page]);
+  }
+}
