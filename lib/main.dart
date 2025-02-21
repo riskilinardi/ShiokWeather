5d51d188ae9d6af7d909581a254e62c705f87088
Nattason Jiradapipat <bewwwjira@Nattasons-MacBook-Air.local>
2025-02-21 17:58:19 +0800

    Change First Page to Login Page and added a camera icon in nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'mappage.dart';
 import 'homepage.dart';
 import 'loginpage.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'profilepage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter_launcher_icons/xml_templates.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
-    ));
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage()));
 
 class MainPage extends StatefulWidget {
   @override
@@ -22,63 +24,70 @@ class MainPage extends StatefulWidget {
 class _MainPage extends State<MainPage> {
   int _page = 1;
   final _pageOpt = [
+    HomePage(),
     MapPage(),
     HomePage(),
     HomePage()
-  ];
+    ];
   GlobalKey _bottomNavigationKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Shiok Weather"),
-          centerTitle: true,
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: <Color>[Colors.lightBlue, Colors.indigo]),
+      appBar: AppBar(
+        title: Text(
+          "Shiok Weather",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold, 
+          ),
+        ),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: <Color>[Colors.lightBlue, Colors.indigo],
             ),
           ),
-          actions: [
-            IconButton(
-              icon: Icon(
-                Icons.account_circle,
-                color: Colors.white,
-                size: 30,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-            ),
-          ],
         ),
-        bottomNavigationBar: CurvedNavigationBar(
-          key: _bottomNavigationKey,
-          index: 1,
-          height: 50.0,
-          items: <Widget>[
-            Icon(MdiIcons.alphaI, size: 30),
-            Icon(MdiIcons.home, size: 30),
-            Icon(MdiIcons.alphaA, size: 30),
-          ],
-          color: Colors.indigo,
-          buttonBackgroundColor: Colors.indigo[400],
-          backgroundColor: Colors.white,
-          animationCurve: Curves.easeInOut,
-          animationDuration: Duration(milliseconds: 600),
-          onTap: (index) {
-            setState(() {
-              _page = index;
-            });
-          },
-          letIndexChange: (index) => true,
-        ),
-        body: _pageOpt[_page]);
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle, color: Colors.white, size: 30),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfilePage()),
+              );
+            },
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: 1,
+        height: 50.0,
+        items: <Widget>[
+          Icon(MdiIcons.camera, size: 30),
+          Icon(MdiIcons.alphaI, size: 30),
+          Icon(MdiIcons.home, size: 30),
+          Icon(MdiIcons.alphaA, size: 30),
+        ],
+        color: Colors.indigo,
+        buttonBackgroundColor: Colors.indigo[400],
+        backgroundColor: Colors.white,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+        letIndexChange: (index) => true,
+      ),
+      body: _pageOpt[_page],
+    );
   }
 }
